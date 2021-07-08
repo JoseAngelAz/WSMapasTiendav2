00628f5e6f45da60c934f5f7e247eaaed9473d8a
José Angel Azucena <angelvanitas555@gmail.com>
2021-07-07 20:13:50 -0600

    Agregamos el generador de token en el UserServicio y nos traimos el secreto a este servicio.Genero el token y lo devuelvo. Si Funciona.

--- WSMapasTiendav2/Servicios/UserServicio.cs
+++ WSMapasTiendav2/Servicios/UserServicio.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using WSMapasTiendav2.Models;
+using WSMapasTiendav2.Models.commons;
 using WSMapasTiendav2.Models.Peticiones;
 using WSMapasTiendav2.Models.Respuestas;
 using WSMapasTiendav2.Tools;
@@ -11,6 +17,14 @@ namespace WSMapasTiendav2.Servicios
 {
     public class UserServicio : IUserServicio
     {
+        //config AppSettigs donde esta el secreto
+        private readonly AppSettings _appSettings;
+
+        public UserServicio(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
         public UsuarioRespuesta Auth(AuthPeticion apeticion)
         {
             //UsuarioRespuesta
@@ -28,8 +42,32 @@ namespace WSMapasTiendav2.Servicios
                 
                 //llenamos la respuesta de UsuarioRespuesta
                 userResp.emailRespuesta = usuario.Email;
+                //Generar el token
+                userResp.TokenRespuesta = GetToken(usuario);
             }
             return userResp;
         }
+        //metodo que duevuelve el token
+        private string GetToken(Usuario usuario)
+        {
+            var manejadorToken = new JwtSecurityTokenHandler();
+            var llave = Encoding.ASCII.GetBytes(_appSettings.Secreto);
+            var TokenDescriptor = new SecurityTokenDescriptor {
+
+               Subject = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, usuario.Id.ToString()),
+                        new Claim(ClaimTypes.Email, usuario.Email)
+                    }
+                ),
+                //opciones de expiracion
+                Expires = DateTime.UtcNow.AddDays(60),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(llave),SecurityAlgorithms.HmacSha256Signature)            
+            };
+            var token = manejadorToken.CreateToken(TokenDescriptor);
+            return manejadorToken.WriteToken(token);
+        }
+
     }
 }
