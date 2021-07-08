3d1d3de32089c47c2b104cda9c25a6688b2eb43b
José Angel Azucena <angelvanitas555@gmail.com>
2021-07-07 19:02:11 -0600

    Agregue el secreto para el token en el json de appsettings y un modelo en common que lo representa llamado AppSettings.

---
+++ WSMapasTiendav2/Models/commons/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSMapasTiendav2.Models.commons
+{
+    public class AppSettings
+    {
+        public string Secreto { get; set; }
+    }
+}
